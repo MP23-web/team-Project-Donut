00e451b8cfa209b31579ce40d640c674da7c4036
DmytroZaburannyi <zaburanniy2011@gmail.com>
2023-02-02 11:50:08 +0200

    commit 1.0

--- src/js/read-more.js
+++
@@ -1,8 +0,0 @@
-// Скрипт для read more/less
-const btn = document.querySelector(".read-more-btn");
-const doptext = document.querySelector(".about-wrapper__text--more");
-btn.addEventListener("click", () => {
-  doptext.classList.toggle("is-open");
-  if (doptext.classList.contains("is-open")) btn.textContent = "Less";
-  else btn.textContent = "Read more";
-});
